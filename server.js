2e249feb13fa51aac4c6b7fd42bf5fe57a6ff09e
Chandana-ghb <komaravoluchandana46@gmail.com>
2026-02-05 19:39:07 +0530

    Change server port from 5000 to 3000

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-import jsonServer from "json-server";
-import cors from "cors";
-
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-server.use(cors());
 server.use(middlewares);
 server.use(router);
 
 server.listen(PORT, () => {
-  console.log(`JSON Server running on port ${PORT}`);
+  console.log("JSON Server running on port", PORT);
 });
